2059ced109dcf066922d2c9db10d6a02b0724b1c
ntritin62 <21522677@gm.uit.edu.vn>
2023-09-15 23:21:18 +0700

    adding main layouts and styling

---
+++ src/pages/requests/ContactCoach.vue
@@ -0,0 +1,4 @@
+<template>
+  <section>123</section>
+  <section>123</section>
+</template>
--- src/router.js
+++ src/router.js
@@ -4,8 +4,9 @@ import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
 import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestReceived from './pages/requests/RequestsReceived.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
@@ -14,10 +15,12 @@ const router = createRouter({
     {
       path: '/coaches/:id',
       component: CoachDetail,
-      children: [{ path: 'contact', component: ContactCoach }],
+      children: [
+        { path: 'contact', component: ContactCoach }, // /coaches/c1/contact
+      ],
     },
     { path: '/register', component: CoachRegistration },
-    { path: '/requests', component: RequestReceived },
+    { path: '/requests', component: RequestsReceived },
     { path: '/:notFound(.*)', component: NotFound },
   ],
 });
