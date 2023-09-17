bede303c6ce87ceba95d47db2ce805bdf4ef736e
ntritin62 <21522677@gm.uit.edu.vn>
2023-09-17 19:36:52 +0700

    fetching coach data

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -26,4 +26,28 @@ export default {
 
     context.commit('registerCoach', { ...coachData, id: userId });
   },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://vue-project-2-bb63d-default-rtdb.europe-west1.firebasedatabase.app/coaches.json`
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      //error
+    }
+
+    const coaches = [];
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+    context.commit('setCoaches', coaches);
+  },
 };
