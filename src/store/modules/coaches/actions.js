ec8f844cd4a584e67d9aa4e04cb23af63fb73895
ntritin62 <21522677@gm.uit.edu.vn>
2023-09-18 22:32:57 +0700

    adding token

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -10,8 +10,11 @@ export default {
       areas: data.areas,
     };
 
+    const token = context.rootGetters.token;
+
     const response = await fetch(
-      `https://vue-project-2-bb63d-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`,
+      `https://vue-project-2-bb63d-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json?auth=` +
+        token,
       {
         method: 'PUT',
         body: JSON.stringify(coachData),
--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -27,8 +27,10 @@ export default {
   },
   async fetchRequests(context) {
     const coachId = context.rootGetters.userId;
+    const token = context.rootGetters.token;
     const response = await fetch(
-      `https://vue-project-2-bb63d-default-rtdb.europe-west1.firebasedatabase.app/requests/${coachId}.json`
+      `https://vue-project-2-bb63d-default-rtdb.europe-west1.firebasedatabase.app/requests/${coachId}.json?auth=` +
+        token
     );
     const responseData = await response.json();
 
